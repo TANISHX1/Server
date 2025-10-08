75bfd694a5b05788a7e55d30d6360c6d1ce58318
tanish <tanishshivhare2@gmail.com>
2025-10-08 22:31:01 +0530

    On branch main  updated functions  Changes to be committed: 	modified:   echo server/header.h

--- echo server/header.h
+++ echo server/header.h
@@ -68,6 +68,7 @@
 
 // custom macros
 #define MSG_SEPRATE "!?!?"
+#define MSG_SEP_LEN strlen(MSG_SEPRATE)
 #define META_BUFFER_SIZE 256
 
 /*
@@ -79,36 +80,38 @@ Meanings of return in directory_create_function :
 
 int create_directory(const char* path)
     {
-    struct stat st = {0};
-    
+    struct stat st = { 0 };
+
     // Check if directory already exists
     if (stat(path, &st) == -1) {
         // Directory doesn't exist, create it
         if (mkdir(path, 0755) == -1) {
             fprintf(stderr, "Error creating directory '%s': %s", path, strerror(errno));
             return -1;
-        }
+            }
         printf("Directory '%s' created successfully.", path);
         return 1; // Created new directory
-    } else {
+        }
+    else {
         // Check if it's actually a directory
         if (S_ISDIR(st.st_mode)) {
             printf("Directory '%s' already exists.", path);
             return 0; // Directory already exists
-        } else {
+            }
+        else {
             fprintf(stderr, "Error: '%s' exists but is not a directory.", path);
             return -1;
+            }
         }
     }
-}
 
 typedef struct client_info {
     char* cli_name;
-    char* MAC;
+    char* cli_uuid;
     int cli_id;
     }client_info;
 
-    // random number generator
+// random number generator
 int random_int()
     {
     return (int)(rand() % 7);
@@ -119,18 +122,36 @@ bool combine_msg(char* msg, char* new_msg)
     // sequence : 1.server name is already present in msg
     // 2.MSG_SEPERATE + clolor_code
     /* BUGs:-
-it is apppending the color code of , when each time clinet connect. fix this  
+it is apppending the color code of , when each time clinet connect. fix this
     */
     int size_dest = strlen(msg), size_src = strlen(new_msg);
-    
+
     if (size_dest + size_src + 1 < META_BUFFER_SIZE) {
         strcat(msg, MSG_SEPRATE);
         strcat(msg, new_msg);
         printf("%s\n", msg);
-        
+
         return true;
         }
     //Final sequence : server name + MSG_SEPERATE + color_code 
+    return false;
+    }
+
+// break meta_data_msg into particular sub_msg
+
+bool break_meta_d(client_info** cli__, char* buffer)
+    {
+    char* ptr;
+    short int size;
+    if ((ptr = strstr(buffer, MSG_SEPRATE)) != NULL) {
+        size = ptr - buffer;
+        // 1.name filling
+        (*cli__)->cli_name = strndup(buffer, size);
+        ptr = ptr + MSG_SEP_LEN;
+        // 2. color_code filling
+        (*cli__)->cli_uuid = strdup(ptr);
+        return true;
+        }
     return false;
     }
 
@@ -140,13 +161,14 @@ void meta_buffer_refresh(char* buffer, int len)
     buffer[len] = '\0';
     }
 
-    // free the client info when client disconnects
-void close_client(void* cli_,int debug)
+// free the client info when client disconnects
+void close_client(void* cli_, int debug)
     {
     client_info* clinet = (client_info*)cli_;
     close(clinet->cli_id);
     free(clinet->cli_name);
+    free(clinet->cli_uuid);
     free(clinet);
-    debug ==3 ? puts("freed clinet info "):(puts(""));
+    (debug == 3) ? puts("freed clinet info ") : (puts(""));
     }
 
